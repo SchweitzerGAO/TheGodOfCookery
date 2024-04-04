8f64f9b800ee62a3bb7a2701b759d6f0f3cd5db2
SchweitzerGAO <1030587461@qq.com>
2024-04-04 11:51:37 +0800

    fix bugs of dataset

--- eval/dataset_upgrade.py
+++ eval/dataset_upgrade.py
@@ -3,15 +3,15 @@ import json
 '''
 Stage 1: extract the questions(run only once)
 '''
-with open('./eval_dataset_test.json','r',encoding='utf-8') as f:
-    questions = [d['conversation'][0]['input'] for d in json.load(f)]
+# with open('./eval_dataset_test.json','r',encoding='utf-8') as f:
+#     questions = [d['conversation'][0]['input'] for d in json.load(f)]
 
 '''
 Stage 2: write the original questions(run only once)
 '''
-with open('./questions.txt','w',encoding='utf-8') as f:
-    for q in questions:
-        f.write(f'{q}\n')
+# with open('./questions.txt','w',encoding='utf-8') as f:
+#     for q in questions:
+#         f.write(f'{q}\n')
 
 '''
 Stage 3: rewrite the questions
@@ -25,6 +25,12 @@ with open('./eval_dataset_test.json','r',encoding='utf-8') as f:
     data = json.load(f)
 with open('./questions_new.txt','r',encoding='utf-8') as f:
     questions = f.readlines()
+
+# ori_questions = [d['conversation'][0]['input'] for d in data]
+
+# for q in ori_questions:
+#     if '\n' in q:
+#         print(q)
 
 for d, q in zip(data, questions):
     d['conversation'][0]['input'] = q
